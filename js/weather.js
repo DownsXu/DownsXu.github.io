6ac340ac5fe8d0f4bbdcf511331094f7ecdd96c3
DownsXu <1798952626@qq.com>
2023-02-23 19:05:40 +0800

    Site updated: 2023-02-23 19:05:39

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "102",
+        "background": "5",
+        "tmpColor": "000000",
+        "tmpSize": "16",
+        "cityColor": "000000",
+        "citySize": "16",
+        "aqiColor": "000000",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "1",
+        "language": "auto",
+        "fixed": "false",
+        "vertical": "top",
+        "horizontal": "left",
+        "key": "a9d753a227274e6988b8305efcabdd12"
+    }
+}
